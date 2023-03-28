28a0c2511e3cb70d5f7fa6725c59582a3a383590
Lucas Marques <lucsilva@pagar.me>
2023-03-28 11:56:33 -0300

    feat: add health check

--- src/apps/LittleCash.Api/Program.cs
+++ src/apps/LittleCash.Api/Program.cs
@@ -1,3 +1,6 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+builder.Services.AddHealthChecksUI().AddInMemoryStorage();;
 
 var app = builder.Build();
 
@@ -16,6 +21,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHealthChecks("/health", new HealthCheckOptions()
+    {
+        Predicate = _ => true,
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    })
+    .UseHealthChecksUI(options => { options.UIPath = "/dashboard"; });
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
