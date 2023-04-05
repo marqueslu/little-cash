5e6e03caeffefeb9f425e9d4832d63f8d8ceb248
nicolas <admin@snico.dev>
2023-04-05 11:45:44 -0300

    fix: hc service configuration

--- src/apps/LittleCash.Api/Program.cs
+++ src/apps/LittleCash.Api/Program.cs
@@ -5,23 +5,35 @@ using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
-var mongoOptions = builder.Configuration.GetOptionsBySection<MongoDatabaseOptions>();
+
+var mongoOptions = builder
+    .Configuration
+    .GetOptionsBySection<MongoDatabaseOptions>();
+
 // Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services
     .AddHealthChecks()
     .AddMongoDb(
-        mongoOptions.ConnectionString,
-        mongoOptions.DatabaseName,
+        mongodbConnectionString: mongoOptions.ConnectionString,
+        name: mongoOptions.DatabaseName,
         failureStatus: HealthStatus.Unhealthy,
         tags: new[] { "database", "mongo" }, TimeSpan.FromSeconds(1));
 
-builder.Services.AddHealthChecksUI().AddInMemoryStorage();
+builder.Services
+    .AddHealthChecksUI(opt =>
+    {
+        opt.SetEvaluationTimeInSeconds(15); //time in seconds between check
+        opt.MaximumHistoryEntriesPerEndpoint(60); //maximum history of checks
+        opt.SetApiMaxActiveRequests(1); //api requests concurrency
+        opt.AddHealthCheckEndpoint("Little Cash Api", "/health");
+    })
+    .AddInMemoryStorage();
 
 var app = builder.Build();
 
@@ -38,10 +50,8 @@ app.UseHealthChecks("/health", new HealthCheckOptions()
         ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
     })
     .UseHealthChecksUI(options => { options.UIPath = "/dashboard"; });
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
