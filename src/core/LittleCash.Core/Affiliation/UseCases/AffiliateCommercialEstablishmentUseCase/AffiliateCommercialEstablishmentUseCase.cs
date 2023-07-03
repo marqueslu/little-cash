b69df54d2a015b5b6fa7726135cc997a7bc104a1
Lucas Marques <lucsilva@pagar.me>
2023-07-03 16:06:09 -0300

    feat: add ExecuteAsync for create EC

--- src/core/LittleCash.Core/Affiliation/UseCases/AffiliateCommercialEstablishmentUseCase/AffiliateCommercialEstablishmentUseCase.cs
+++ src/core/LittleCash.Core/Affiliation/UseCases/AffiliateCommercialEstablishmentUseCase/AffiliateCommercialEstablishmentUseCase.cs
@@ -1,16 +1,37 @@
-﻿using LittleCash.Core.Shared.UseCases;
+﻿using LittleCash.Api.Exceptions;
+using LittleCash.Core.Affiliation.Repositories;
+using LittleCash.Core.Entities;
+using LittleCash.Core.Shared.UseCases;
 
 namespace LittleCash.Core.Affiliation.UseCases.AffiliateCommercialEstablishmentUseCase;
 
 public class AffiliateCommercialEstablishmentUseCase : IUseCase<AffiliateCommercialEstablishmentUseCaseRequest,
-    AffiliateCommercialEstablishmentUseCaseResponse>    
+    AffiliateCommercialEstablishmentUseCaseResponse>
 {
-    public Task<AffiliateCommercialEstablishmentUseCaseResponse> ExecuteAsync(
-        AffiliateCommercialEstablishmentUseCaseRequest request)
+    private readonly ICommercialEstablishmentRepository _commercialEstablishmentRepository;
+
+    public AffiliateCommercialEstablishmentUseCase(ICommercialEstablishmentRepository commercialEstablishmentRepository)
     {
-        return Task.FromResult(new AffiliateCommercialEstablishmentUseCaseResponse
+        _commercialEstablishmentRepository = commercialEstablishmentRepository;
+    }
+
+    public async Task<AffiliateCommercialEstablishmentUseCaseResponse> ExecuteAsync(
+        AffiliateCommercialEstablishmentUseCaseRequest request, CancellationToken cancellationToken)
+    {
+        var existentCommercialEstablishment =
+            await _commercialEstablishmentRepository.GetByDocumentNumberAsync(request.Document, cancellationToken);
+
+        if (existentCommercialEstablishment is not null)
+            throw new ConflictException("Already exists a commercial establishment with this document");
+
+        var commercialEstablishment =
+            new CommercialEstablishment(request.Name, request.Document, request.PaymentScheme, request.Domicile);
+
+        await _commercialEstablishmentRepository.CreateAsync(commercialEstablishment, cancellationToken);
+
+        return new AffiliateCommercialEstablishmentUseCaseResponse
         {
-            Teste = 1
-        });
+            Success = true
+        };
     }
 }
