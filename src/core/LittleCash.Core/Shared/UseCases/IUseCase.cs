50a405398d3e6242817c6241686305dc8bb97696
Lucas Marques <lucsilva@pagar.me>
2023-07-03 16:10:22 -0300

    fix: add cancellation token

--- src/core/LittleCash.Core/Shared/UseCases/IUseCase.cs
+++ src/core/LittleCash.Core/Shared/UseCases/IUseCase.cs
@@ -4,5 +4,5 @@ public interface IUseCase<TRequest, TResponse>
     where TRequest : class, IUseCaseRequest
     where TResponse : class, IUseCaseResponse
 {
-    Task<TResponse> ExecuteAsync(TRequest request);
+    Task<TResponse> ExecuteAsync(TRequest request, CancellationToken cancellationToken);
 }
