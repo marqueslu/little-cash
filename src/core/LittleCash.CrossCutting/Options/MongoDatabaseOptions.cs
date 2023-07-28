a9d94e9f3cd55f94f62877fc61d98247027f6bad
Lucas Marques <lucsilva@pagar.me>
2023-07-28 16:07:59 -0300

    fix: remove file

--- src/core/LittleCash.CrossCutting/Options/MongoDatabaseOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace LittleCash.CrossCutting.Options;
-
-public class MongoDatabaseOptions
-{
-    public string ConnectionString { get; init; } = default!;
-    public string DatabaseName { get; set; } = default!;
-}
